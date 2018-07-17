e9aba563fc55c00abbbb973797ce417ff7e932f0
Vasilis <41332451+vasilomanolis@users.noreply.github.com>
2018-07-17 15:08:51 +0100

    Update DBHelper.java

--- src/Database/DBHelper.java
+++ src/Database/DBHelper.java
@@ -13,9 +13,9 @@ public class DBHelper {
 	
 	//Connection details.
 	private static final String driver="org.postgresql.Driver";
-	private static final String url="jdbc:postgresql://mod-msc-sw1.cs.bham.ac.uk:5432/chicago";
-	private static final String DBusername="chicago";
-	private static final String DBpassword="v74x4mkdhl";
+	private static final String url="jdbc:postgresql://link";
+	private static final String DBusername="username";
+	private static final String DBpassword="password";
 
 	private static  Connection con=null;
 	static
